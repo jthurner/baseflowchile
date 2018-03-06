560934ebb5fcf5a30f91d869312a263dbd404fff
Joschka Thurner <mail@joschkathurner.de>
2018-03-06 13:07:47 +0100

    Update CAMELS-CL to 2018-03-05

--- README.md
+++ README.md
@@ -1,25 +1,34 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
-baseflowchile
-=============
 
-Pre-processed streamflow data (1913-2016) from Chile for baseflow-analysis. Contains helper functions to calculate gap statistics for selected date ranges.
+# baseflowchile
 
-Install from github with `devtools::install_github("jthurner/baseflowchile")`.
+Pre-processed streamflow data (1913-2016) from Chile for
+baseflow-analysis. Contains helper functions to calculate gap statistics
+for selected date ranges.
+
+Install from github with
+`devtools::install_github("jthurner/baseflowchile")`.
 
 Created at [ITT Cologne](http://www.tt.th-koeln.de/).
 
-Data Source
------------
+## Data Source
 
-*Alvarez-Garreton, C., Mendoza, P., Boisier, J. P., Galleguillos, M., Zambrano-Bigiarini, M., Lara, A., Addor, N., Puelma, C., Cortes, G., Garreaud, R., and McPhee, J.:* The CAMELS-CL dataset: catchment attributes and meteorology for large sample studies – Chile dataset, to be submitted to Hydrol. Earth Syst. Sci.
+*Alvarez-Garreton, C., Mendoza, P., Boisier, J. P., Galleguillos, M.,
+Zambrano-Bigiarini, M., Lara, A., Addor, N., Puelma, C., Cortes, G.,
+Garreaud, R., and McPhee, J.:* [The CAMELS-CL dataset: catchment
+attributes and meteorology for large sample studies – Chile dataset, to
+be submitted to Hydrol. Earth Syst.
+Sci.](https://doi.org/10.5194/hess-2018-23)
 
-[Download Link](http://www.cr2.cl/recursos-y-publicaciones/bases-de-datos/datos-informacion-integrada-por-cuencas/)
+The original dataset can be downloaded from
+[here](http://www.cr2.cl/recursos-y-publicaciones/bases-de-datos/datos-informacion-integrada-por-cuencas/).
 
-Usage
------
+## Usage
 
-The package contains cleaned-up streamflow data in mm and associated catchment characteristics from the CAMELS-CL dataset (the script used for preprocessing can be found in the source package).
+The package contains cleaned-up streamflow data in mm and associated
+catchment characteristics from the CAMELS-CL dataset (the script used
+for preprocessing can be found in the source package).
 
 ``` r
 library(baseflowchile)
@@ -31,7 +40,10 @@ zoo::read.zoo(as.data.frame(cl_streamflow_mm))
 cl_catchment_md
 ```
 
-The function gap\_statistics() adds daily and monthly completeness percentages (c\_d, c\_m) and the longest monthly gap (l\_g) to the metadata table. Gap analysis is performed seperately for the specified date ranges.
+The function gap\_statistics() adds daily and monthly completeness
+percentages (c\_d, c\_m) and the longest monthly gap (l\_g) to the
+metadata table. Gap analysis is performed seperately for the specified
+date ranges.
 
 ``` r
 date_ranges <- c("1985-1994","1995-2004","2005-2015","1985-2015")
@@ -42,7 +54,13 @@ cl_md_gaps
 # cl_md_gaps <- filter(cl_md_gaps, elevation >= 700)
 ```
 
-With the gap statistics added to the metadata, extract\_ts() filters out stations with certain gap characteristics for each date range and returns the resulting streamflow time series as well as separate metadata tables containing only the stations used. Optionally, the metadata tables can be written out as an excel file and the time series as seperate csv files per station (sorted into directories for each date range).
+With the gap statistics added to the metadata, extract\_ts() filters out
+stations with certain gap characteristics for each date range and
+returns the resulting streamflow time series as well as separate
+metadata tables containing only the stations used. Optionally, the
+metadata tables can be written out as an excel file and the time series
+as seperate csv files per station (sorted into directories for each date
+range).
 
 ``` r
 # for each date range, consider only stations which have at least 80% of data (daily + monthly) and no 
--- data-raw/preprocessing.R
+++ data-raw/preprocessing.R
@@ -1,15 +1,15 @@
 library(dplyr)
+library(tidyr)
 library(readr)
 library(devtools)
 library(stringr)
 
-
 ################################################################################
 # The following files are not included in git and have to be dropped into      #
 # data-raw manually:                                                           #
-#   - 3_Catchment_streamflow_mm.csv                                            #
-#   - 1_Catchment_attributes.csv                                               #
-#   - cr2_qflxDaily_2017_stations.txt                                          #
+#   - 3_CAMELScl_streamflow_mm.csv, 2018-01-29                                 #
+#   - 1_Catchment_attributes.csv, 2018-01-29                                   #
+#   - cr2_qflxDaily_2018_stations.txt, 2018-01-10                              #
 #                                                                              #
 # Source:                                                                      #
 # http://www.cr2.cl/recursos-y-publicaciones/bases-de-datos/datos-de-caudales/ #
@@ -18,26 +18,30 @@ library(stringr)
 ################################################################################
 
 
-
 ################################################################################
 # Reformat discharge data into multivariate series readable by zoo             #
 ################################################################################
+streamflow_mm_src <- "data-raw/3_CAMELScl_streamflow_mm.csv"
 
-cl_streamflow_mm <- read_csv("data-raw/3_Catchment_streamflow_mm.csv") %>%
-  mutate(Date=as.Date(paste(year,month,day,sep="-"))) %>%
-  select(Date,everything(),-c(X1,year,month,day)) %>%
-  mutate_at(vars(-Date),as.double) %>%
-  mutate_at(vars(-Date),round,10) %>%
-  # prefix station codes
-  rename_at(vars(-Date),funs(if_else(. < 1000000,
-                                     paste0("q0", .),
-                                     paste0("q", .))))
+# read column names
+header <- scan(streamflow_mm_src, what="",nlines = 1, sep=",") %>%
+  str_replace(" ", "0") %>%
+  paste0("q",.) %>%
+  magrittr::inset(1, "Date")
+
+# read data
+cl_streamflow_mm <- read_csv(
+  streamflow_mm_src,
+  col_names = header,
+  col_types = cols(Date = col_date(format = "%Y-%m-%d"), .default = "d"),
+  skip = 8)
 
 # subset to remove leading and trailing all(NA) rows
 ixna <- apply(cl_streamflow_mm[,-1],1,function(x) all(is.na(x)))
 ixmin <- min(which(ixna == FALSE))
 ixmax <- max(which(ixna == FALSE))
 cl_streamflow_mm <- cl_streamflow_mm[ixmin:ixmax,]
+
 # save to disk
 use_data(cl_streamflow_mm, overwrite=TRUE)
 
@@ -45,33 +49,32 @@ use_data(cl_streamflow_mm, overwrite=TRUE)
 # Compile Metadata                                                             #
 ################################################################################
 
-# read station metadata to get elevation, start, end
-md_stations <- read_csv("data-raw/cr2_qflxDaily_2017_stations.txt") %>%
-  select(stationcode=codigo_estacion, elevation=altura,
-         start=inicio_observaciones,end=fin_observaciones)
+# read discharge station metadata to get elevation, start, end
+md_stations <- read_csv("data-raw/cr2_qflxDaily_2018_stations.txt",
+                        cols_only(codigo_estacion = "c",
+                                  inicio_observaciones = col_date(),
+                                  fin_observaciones = col_date()),
+                                  col_names = TRUE) %>%
+  rename(stationcode=codigo_estacion, q_start=inicio_observaciones,
+         q_end=fin_observaciones)
 
-# some stations have elevation set to 0, which should be NA
-# elevation for these stations has been extracted from DEM
-missing_elevation <- read_csv("data-raw/missing-elev-from-DEM.csv") %>%
-  transmute(stationcode=stationcod,elevation_DEM=elevation)
-
-# stitching everything together
-cl_catchment_md <- read_csv("data-raw/1_Catchment_attributes.csv") %>%
-  rename(stationcode=gage_id, station_name=gage_name,latitude=gage_lat,
-         longitude=gage_lon) %>%
-  # add main river and river segment
-  mutate(main_river = as.integer(str_sub(stationcode, end=-6)),
-         river_segment = as.integer(str_sub(stationcode,-5,-4))) %>%
-  # add elevation, start, end from station metadata file
+# reformat CAMELS CL metadata
+cl_catchment_md <- read_csv("data-raw/1_Catchment_attributes.csv",
+                            cols(.default="c"), col_names=TRUE) %>%
+  # save table to keep order of variables
+  {. ->> table_original} %>%
+  # transpose
+  gather(stationcode, val, 2:ncol(.)) %>%
+  spread(names(.)[1], 'val') %>%
+  # add  q_start, q_end from station metadata file
   left_join(md_stations) %>%
-  # add missing elevation
-  left_join(missing_elevation) %>%
-  mutate(elevation=coalesce(na_if(elevation,0),elevation_DEM)) %>%
-  select(-elevation_DEM) %>%
   # reorder
-  select(-starts_with("lc"),-starts_with("wu"),everything()) %>%
+  select(stationcode, pull(table_original,1),q_end, q_start) %>%
+  # rename
+  rename(station_name=gauge_name,latitude=gauge_lat,
+          longitude=gauge_lon, elev_station=elev_gauge) %>%
   # prefix stationcode
-  mutate(sc1 = if_else(stationcode<10000000,
+  mutate(stationcode = if_else(as.integer(stationcode) < 10000000,
                        paste0("q0",stationcode),
                        paste0("q",stationcode)))
 # save metadata
